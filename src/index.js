696109ea12691953623079138fd07a464f2c7e9c
snooze26 <langdon.douglas26@gmail.com>
2025-02-11 11:20:55 -0600

    capture weather icon in json then added the icon to dom based on weather

--- src/index.js
+++ src/index.js
@@ -2,31 +2,38 @@ import { fetchWeather } from "./fetchApi.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("#form");
-    const table = document.querySelector("#weatherDataBody");
 
     form.addEventListener("submit", async (event) => {
         event.preventDefault();
         const locationInput = document.querySelector("#searchInput").value;
 
-        console.log(locationInput);
 
-        const weatherData = await fetchWeather(locationInput);
+        const result = await fetchWeather(locationInput);
+        console.log("results: ", result);
+
+        const {reportFields, weatherIcons} = result; 
+        console.log("WD", reportFields);
+        console.log("WI: ", weatherIcons);
+
 
         // retrieve data row cells
         const temp = document.querySelector("#tempData");
-        temp.textContent = `${weatherData.temp}\u00B0F`;
+        temp.textContent = `${reportFields.temp}\u00B0F`;
 
         const feelsLike = document.querySelector("#feelsLikeData");
-        feelsLike.textContent = `${weatherData.feelsLike}\u00B0F`;
+        feelsLike.textContent = `${reportFields.feelsLike}\u00B0F`;
         
         const dateTime = document.querySelector("#dateTimeData");
-        dateTime.textContent = `${weatherData.dateTime}`;
+        dateTime.textContent = `${reportFields.dateTime}`;
 
         const description = document.querySelector("#descriptionData");
-        description.textContent = `${weatherData.description}`;
+        description.textContent = `${reportFields.description}`;
 
         const percipType = document.querySelector("#percipTypeData");
-        percipType.textContent = `${weatherData.percipType}`;
+        percipType.textContent = `${reportFields.percipType}`;
 
+        const weatherIcon = document.querySelector("#weatherIcon");
+        weatherIcon.src = weatherIcons[reportFields.icon] || "icons/default.svg";
+        weatherIcon.alt = `${reportFields.icon};`
     });
 })
